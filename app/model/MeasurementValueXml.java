f693e0f3c7a0e4107cceb719a21b217532c0b122
hansjuergnett <goeginett@hotmail.com>
2017-06-17 15:40:41 +0200

    Add id to MeasurementValueXml

--- app/model/MeasurementValueXml.java
+++ app/model/MeasurementValueXml.java
@@ -7,10 +7,19 @@ package model;
  */
 public class MeasurementValueXml {
 
+    String id = "";
     String value = "";
     long time = 0;
 
     public MeasurementValueXml() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getValue() {
