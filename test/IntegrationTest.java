860977be2c6899df1292b0af137f3a963eb2bad0
Patrick Stoffel <patrick_stoffel@hotmail.com>
2017-07-03 00:36:37 +0200

    modified IntegrationTest

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,24 +1,36 @@
 import org.junit.*;
 
-import play.mvc.*;
-import play.test.*;
-
 import static play.test.Helpers.*;
 import static org.junit.Assert.*;
 
-import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
 
     /**
      * integration test
-     * just check if the device-liste page is being shown
+     * just check if the device-liste (list, update and delete) page is being shown
      */
     @Test
     public void testDeviceListPage() {
         running(testServer(9000, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
             browser.goTo("http://localhost:9000/list");
-            assertTrue(browser.pageSource().contains("Device-Liste"));
+            assertTrue(browser.pageSource().contains("Device-Liste")); //erwarteter Inhalt "Device-Liste"
+        });
+    }
+
+    @Test
+    public void testDeviceUpdatePage() {
+        running(testServer(9000, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:9000/device/update/device1");
+            assertTrue(browser.pageSource().contains("update")); //erwarteter Inhalt "update"
+        });
+    }
+
+    @Test
+    public void testDeviceDeletePage() {
+        running(testServer(9000, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:9000/device/delete/device1");
+            assertTrue(browser.pageSource().contains("delete")); //erwarteter Inhalt "delete"
         });
     }
 
