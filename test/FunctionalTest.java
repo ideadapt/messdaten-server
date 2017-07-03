f1ed8353af0078477fbd79d83018a666a936cef1
Patrick Stoffel <patrick_stoffel@hotmail.com>
2017-07-03 23:58:12 +0200

    modified FunctionalTest, add DeviceValidatorTest

--- test/FunctionalTest.java
+++ test/FunctionalTest.java
@@ -1,14 +1,6 @@
-import model.Device;
 import play.mvc.Result;
-import services.DeviceValidator;
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.*;
-
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
-
 import static play.test.Helpers.*;
 import play.mvc.Http.RequestBuilder;
 
@@ -43,49 +35,4 @@ public class FunctionalTest {
             assertEquals("utf-8", result.charset().get());
         });
     }
-
-
-    //Testing DeviceValidator
-    @Test
-    public void validateDeviceWithValidReturnsTrue() {
-
-        List<Device> devices = new ArrayList<>();
-        devices.add(new Device("device1", "192.230.25.26", "c:/temp/values.txt", "group1", "txt"));
-        devices.add(new Device("device2", "192.230.25.26", "c:/temp/values.txt", "group2", "xml"));
-        devices.add(new Device("device3", "192.230.25.26", "c:/temp/values.txt", "group1", "txt"));
-        Device newDevice = new Device("device4", "192.230.25.26", "c:/temp/values.txt", "group3", "xml");
-
-        boolean valid = DeviceValidator.validateDevice(devices, newDevice);
-
-        assertTrue("Valid device returns true", valid);
-    }
-
-    @Test
-    public void validateDeviceWithExistingNameReturnsFalse() {
-
-        List<Device> devices = new ArrayList<>();
-        devices.add(new Device("device1", "192.230.25.26", "c:/temp/values.txt", "group1", "txt"));
-        devices.add(new Device("device2", "192.230.25.26", "c:/temp/values.txt", "group2", "xml"));
-        devices.add(new Device("device3", "192.230.25.26", "c:/temp/values.txt", "group1", "txt"));
-        Device newDevice = new Device("device1", "192.230.25.26", "c:/temp/values.txt", "group3", "xml");
-
-        boolean valid = DeviceValidator.validateDevice(devices, newDevice);
-
-        assertFalse("Device with existing name returns false", valid);
-    }
-
-    @Test
-    public void validateDeviceWithEmptyStringNameReturnsFalse() {
-
-        List<Device> devices = new ArrayList<>();
-        devices.add(new Device("device1", "192.230.25.26", "c:/temp/values.txt", "group1", "txt"));
-        devices.add(new Device("device2", "192.230.25.26", "c:/temp/values.txt", "group2", "xml"));
-        devices.add(new Device("device3", "192.230.25.26", "c:/temp/values.txt", "group1", "txt"));
-        Device newDevice = new Device("", "192.230.25.26", "c:/temp/values.txt", "group3", "xml");
-
-        boolean valid = DeviceValidator.validateDevice(devices, newDevice);
-
-        assertFalse("Device with empty string name returns false", valid);
-    }
-
 }
